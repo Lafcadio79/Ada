80b94a58997edf71f70695c562a929e127f86238
Dimitri Belli <dimitribelli@hotmail.it>
2021-01-25 16:37:18 +0100

    Update New_Type.ads

--- Customize_Types/New_Type.ads
+++ Customize_Types/New_Type.ads
@@ -5,7 +5,11 @@
 -- -----------------------------------------------------------
 
 package New_Type is
-
+	
+    -- a type declaration consists of the reserved word "type", 
+    -- the identifier to be associated with the type,
+    -- the reserved word "in", and the definition of the type
+    -- followed by the terminating semicolon
     type Colour is (Red, Amber, Green);
 
 function Print_Colour (Col: Colour) return Colour;
