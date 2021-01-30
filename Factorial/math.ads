56c28c39e77c24d308ad3e2176b3ec91c2182083
Dimitri Belli <dimitribelli@hotmail.it>
2021-01-30 16:36:01 +0100

    Update math.ads

--- Factorial/math.ads
+++ Factorial/math.ads
@@ -1,3 +1,9 @@
+-- -----------------------------------------------------------
+-- |                                                          |
+-- |        The package with the function signature           |
+-- |                                                          |
+-- -----------------------------------------------------------
+
 package math is
 
 function Factorial(N: Positive) return Positive;
